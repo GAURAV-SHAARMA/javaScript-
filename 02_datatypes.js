392b3daf8007ce6295c8f5cc58f304fe257a9c55
GAURAV-SHAARMA <gauravsharma1913@gmail.com>
2026-09-20 14:49:30 +0530

    Add 02_datatypes.js with variable declarations and type logging

---
+++ 02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello gaurav");
+// we are using node js so it will give error in terminal it will be used in browser
+
+console.log(3 + 3)// code readability should be high
+console.log("hello gaurav")
+
+// ecma Scipt documentation is used to learn js
+
+let name= " gaurav" // string
+let age = 18 // number 
+
+let isloogin = true // boolean
+let state; // undefined
+
+
+// number ->> 2^53
+//bigint 
+//string->> '' or ""
+// boolean -> true or false
+// null ->> standalone value
+// undefined ->> value is not assigned
+// symbol ->> unique and immutable
+
+//object ->> key value pair
+
+console.log(typeof "gaurav")
+console.log(typeof undefined) // undefined
+
+console.log(typeof null)// object
