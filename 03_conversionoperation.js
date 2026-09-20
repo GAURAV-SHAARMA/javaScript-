6d3e6aa59a48c33c8a5da19308ac9a33c307a967
GAURAV-SHAARMA <gauravsharma1913@gmail.com>
2026-09-20 14:58:36 +0530

    Add 03_conversionoperation.js for type conversion examples in JavaScript

---
+++ 03_conversionoperation.js
@@ -0,0 +1,30 @@
+let score = "33aa"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+
+let valueInNumber = Number(score)// now that converted in to number in javascript but gives NaN on running in terminal
+console.log(typeof valueInNumber) 
+console.log(valueInNumber) 
+
+
+// "33" => 33
+//"33abc"=> NaN
+//true => 1 and false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn) 
+//1 ->> true
+//"" ->> false
+//"hitesh" -> true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
